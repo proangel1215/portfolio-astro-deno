f8503c42a158564c2149e978d2e4e9cbcefde4fc
proangel <proangel1215@gmail.com>
2024-09-26 20:33:04 +0200

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import deno from '@deno/astro-adapter';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  output: 'server',
+  adapter: deno(),
+  integrations: [tailwind(), react()],
 });
